f15f98c9846738904a513fc71092bf22fe9a60b5
Jon <jonathanjackson424@gmail.com>
2019-11-19 16:55:48 -0500

    almost there

--- src/Folders.js
+++ src/Folders.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Folder from './Folder';
+import { NavLink } from 'react-router-dom';
 
 function Folders(props) {
   // console.log(props);
   
   const files = props.folders.map((folder, i) => 
+    <NavLink to={`/folder/${folder.id}`}>
     <Folder id={folder.id} name={folder.name} click={props.click} 
-      
-      /> 
+      /> </NavLink>
       )
 
     return (
--- src/Notes.js
+++ src/Notes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Note from './Note';
 
 export default function Notes(props) {
-  console.log(props.folderIds);
   const text = props.notes
   .filter(note => !props.selected || (note.folderId === props.selected))
   .map((note, i) => 
